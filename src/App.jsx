a2469fd683a23bd30fbdfe34bdf0166d1dd21f7c
jayveersingh3031 <jayveersinghpawar03@gmail.com>
2024-08-08 12:16:16 +0530

    miniContext

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import UserContextProvider from "./UserContextProvider/UserContextProvider"
+import Login from "./components/Login"
+import Profile from "./components/Profile"
+import "./App.css"
+function App() {
+  return(
+    <UserContextProvider>
+    <h1>Hello World</h1>
+    <Login/>
+    <Profile/>
+  </UserContextProvider>
+  )
+}
+
+export default App;
